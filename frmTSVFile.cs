836c67e76b611aa78b646ed99e12123917440ddc
ChihYu0831 <chyulola434@gmail.com>
2026-05-14 22:23:44 +0800

    新增單字清單即時搜尋功能
    於「單字清單」視窗右側工具列新增搜尋輸入框，支援即時依據「單字」或「解釋」欄位過濾顯示內容。UpdateListView 方法加入關鍵字參數，實作忽略大小寫的篩選邏輯，並處理預設提示文字顯示。提升大量單字資料的檢索效率與使用體驗。

--- frmTSVFile.cs
+++ frmTSVFile.cs
@@ -23,6 +23,8 @@ namespace TSVFile
         /// </summary>
         WordCollection _WordList = new WordCollection();
 
+        private ToolStripTextBox tsTxtSearch;
+
         public frmTSVFile()
         {
             InitializeComponent();
@@ -31,23 +33,30 @@ namespace TSVFile
         /// <summary>
         /// 更新 ListView 的內容
         /// </summary>
-        private void UpdateListView()
+        private void UpdateListView(string keyword = "")
         {
             lvwWord.BeginUpdate(); //暫停重繪
             // 清除 ListView 的所有項目
             lvwWord.Items.Clear();
+
             // 將 WordCollection 物件中的資料載入到 ListView 中
             foreach (WordItem item in _WordList)
             {
-                // 建立 ListViewItem 物件
-                ListViewItem lvi = new ListViewItem(item.Word);
-                lvi.SubItems.Add(item.Phonogram);
-                lvi.SubItems.Add(item.SoundPath);
-                lvi.SubItems.Add(item.Explain);
-                // 將 ListViewItem 物件加入到 ListView 中
-                lvwWord.Items.Add(lvi);
+                // 篩選邏輯：如果 keyword 是空的，或者「單字」或「解釋」中包含 keyword (忽略大小寫)，就顯示它
+                if (string.IsNullOrWhiteSpace(keyword) ||
+                    item.Word.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                    item.Explain.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    // 建立 ListViewItem 物件
+                    ListViewItem lvi = new ListViewItem(item.Word);
+                    lvi.SubItems.Add(item.Phonogram);
+                    lvi.SubItems.Add(item.SoundPath);
+                    lvi.SubItems.Add(item.Explain);
+                    // 將 ListViewItem 物件加入到 ListView 中
+                    lvwWord.Items.Add(lvi);
+                }
             }
-            lvwWord.EndUpdate(); //重繪;
+            lvwWord.EndUpdate(); //重繪
         }
 
         // 在您讀取完檔案並將所有項目加入 ListView(lvwWord) 之後，呼叫此方法
@@ -113,6 +122,43 @@ namespace TSVFile
         private void frmTSVFile_Load(object sender, EventArgs e)
         {
             this.tsslMessage.Text = "";
+
+            // 建立搜尋的 TextBox
+            tsTxtSearch = new ToolStripTextBox();
+            tsTxtSearch.Alignment = ToolStripItemAlignment.Right; // 靠右對齊
+            tsTxtSearch.Width = 200; // 控制寬度
+            tsTxtSearch.ToolTipText = "在此輸入單字或解釋進行搜尋...";
+            tsTxtSearch.Text = "搜尋...";
+            tsTxtSearch.ForeColor = Color.Gray;
+
+            // 加入文字焦點事件，實作 Placeholder (預設文字) 的效果
+            tsTxtSearch.GotFocus += (s, args) =>
+            {
+                if (tsTxtSearch.Text == "搜尋...")
+                {
+                    tsTxtSearch.Text = "";
+                    tsTxtSearch.ForeColor = Color.Black;
+                }
+            };
+            tsTxtSearch.LostFocus += (s, args) =>
+            {
+                if (string.IsNullOrWhiteSpace(tsTxtSearch.Text))
+                {
+                    tsTxtSearch.Text = "搜尋...";
+                    tsTxtSearch.ForeColor = Color.Gray;
+                }
+            };
+
+            // 加入 TextChanged 事件，當使用者輸入文字時自動過濾
+            tsTxtSearch.TextChanged += (s, args) =>
+            {
+                // 如果是預設提示字串，則以空字串進行過濾 (顯示全部)
+                string searchString = (tsTxtSearch.Text == "搜尋...") ? "" : tsTxtSearch.Text;
+                UpdateListView(searchString);
+            };
+
+            // 將搜尋方塊加入現有的 MenuStrip
+            mnsWord.Items.Add(tsTxtSearch);
         }
     }
 }
